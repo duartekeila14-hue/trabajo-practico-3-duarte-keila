3eeda68827836e58c8f0bc471d430b419ce4571b
duarte-serena <duartekeila14@gmail.com>
2026-06-11 00:24:48 -0300

    Agregadas dos funciones asincronas, una para traer la lista de los personajes desde la API y otra para mostrar los detalles de un personaje segun su ID

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+const listarPersonajes = async () => {
+    try {
+        const response = await fetch('https://thesimpsonsapi.com/api/characters');
+        const data = await response.json()
+        return data.results
+    } catch (error) {
+        console.log(error, 'Error al obtener los personajes');
+        throw error;
+    }
+}
+const personajes = await listarPersonajes()
+
+const detallePersonaje = async (id) => {
+    try {
+        const res = await fetch(`https://thesimpsonsapi.com/api/characters/${id}`)
+        const data = await res.json()
+        return data
+    } catch (error) {
+        console.log(error, 'Error al obtener el detalle del personaje')
+    }
+}
